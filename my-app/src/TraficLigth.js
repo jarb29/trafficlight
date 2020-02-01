016829854e951d04130c332a9298768bc43f1dd4
Alex first test <jarb29@gmail.com>
2020-02-01 20:47:34 -0300

    css done

---
+++ my-app/src/TraficLigth.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+
+export default function TrafficLigth() {
+    return (
+        <div class = "square" >
+            <div id="svg1" >
+                <circle id= "circle1" />
+            </div>
+            <div id="svg2">
+                <circle id="circle2"/>
+            </div>
+            <div id="svg3">
+                <circle id="circle3"/>
+            </div>
+        </div>
+    );
+    }
