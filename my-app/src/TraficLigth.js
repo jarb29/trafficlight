ee5159e6803490e95f1c79909500f83bd6b4b394
Alex first test <jarb29@gmail.com>
2020-02-02 09:25:46 -0300

    final

--- my-app/src/TraficLigth.js
+++ my-app/src/TraficLigth.js
@@ -3,18 +3,35 @@ import React from 'react';
 
 
 
-export default function TrafficLigth() {
-    return (
-        <div class = "square" >
-            <div id="svg1" >
-                <circle id= "circle1" />
-            </div>
-            <div id="svg2">
-                <circle id="circle2"/>
-            </div>
-            <div id="svg3">
-                <circle id="circle3"/>
-            </div>
+export default class TrafficLigth extends React.Component{
+  
+        constructor(){
+            super();
+            this.state ={
+                click:null
+            };
+        }
+        render (){
+
+            console.log(this.state)
+            let redExtra = '';
+            if(this.state.click==='red') redExtra ="selected";
+            let yelloExtra = '';
+            if(this.state.click==='yellow') yelloExtra ="selected";
+            let greenExtra = '';
+            if(this.state.click==='green') greenExtra ="selected";
+
+            return(  
+        <>
+        <div id="trafficTop"></div>
+        <div id="container">
+              <div className={"red light " + redExtra} onClick={()=>this.setState({click:'red'})}></div>
+              <div className= {"yellow light " + yelloExtra} onClick={()=>this.setState({click:'yellow'})}></div>
+              <div className= {"green light" + greenExtra} onClick={()=>this.setState({click:'green'})}></div>
         </div>
-    );
+        </>
+            )
     }
+    }
+
+  
